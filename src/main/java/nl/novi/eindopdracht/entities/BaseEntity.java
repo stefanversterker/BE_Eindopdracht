c4fe1b943581322dbe36f5f0600e997ecbe95e55
stefanversterker <stefanversterker@gmail.com>
2026-07-04 13:02:42 +0200

    created BaseEntity entity

--- src/main/java/nl/novi/eindopdracht/entities/BaseEntity.java
+++ src/main/java/nl/novi/eindopdracht/entities/BaseEntity.java
@@ -1,4 +1,24 @@
 package nl.novi.eindopdracht.entities;
 
-public class BaseEntity {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
