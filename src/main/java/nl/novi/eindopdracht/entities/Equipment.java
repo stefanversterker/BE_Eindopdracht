6c489a0aef6b0e32b36590fb7c770196f8bcaa0c
stefanversterker <stefanversterker@gmail.com>
2026-07-04 13:10:03 +0200

    created Equipment entity

--- src/main/java/nl/novi/eindopdracht/entities/Equipment.java
+++ src/main/java/nl/novi/eindopdracht/entities/Equipment.java
@@ -1,4 +1,30 @@
 package nl.novi.eindopdracht.entities;
 
-public class Equipment {
+import jakarta.persistence.*;
+
+//Here I use @Entity instead of @MappedSuperclass because I want to enable users to request an Equipment Table, so it's more than just a superclass.
+@Entity
+public abstract class Equipment extends BaseEntity {
+
+    private String brand;
+
+    private String model;
+
+    //Getters and setters
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
