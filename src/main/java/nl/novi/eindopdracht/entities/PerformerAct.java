0e1f09869f8865c54613af4bf63e8518722a89fd
stefanversterker <stefanversterker@gmail.com>
2026-07-04 13:19:08 +0200

    created PerformerAct entity

--- src/main/java/nl/novi/eindopdracht/entities/PerformerAct.java
+++ src/main/java/nl/novi/eindopdracht/entities/PerformerAct.java
@@ -1,4 +1,49 @@
 package nl.novi.eindopdracht.entities;
 
-public class PerformerAct {
+import jakarta.persistence.*;
+
+@Entity
+// I used the @UniqueConstraint because performer_id - act_id pairs should only be allowed to occur once
+@Table(
+        name = "performer_acts",
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = {"performer_id", "act_id"}
+        )
+)
+
+public class PerformerAct extends BaseEntity{
+
+    @ManyToOne
+    @JoinColumn(name = "performer_id")
+    private PerformerProfile performer;
+
+    @ManyToOne
+    @JoinColumn(name = "act_id")
+    private Act act;
+
+    private String role;
+
+    public PerformerProfile getPerformer() {
+        return performer;
+    }
+
+    public void setPerformer(PerformerProfile performer) {
+        this.performer = performer;
+    }
+
+    public Act getAct() {
+        return act;
+    }
+
+    public void setAct(Act act) {
+        this.act = act;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
