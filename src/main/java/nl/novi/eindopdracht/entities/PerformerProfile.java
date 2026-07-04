118d47c8a343b0e2191ddf1be43dc83ff2dc92ef
stefanversterker <stefanversterker@gmail.com>
2026-07-04 15:05:01 +0200

    created PerformerProfile entity

--- src/main/java/nl/novi/eindopdracht/entities/PerformerProfile.java
+++ src/main/java/nl/novi/eindopdracht/entities/PerformerProfile.java
@@ -1,4 +1,46 @@
 package nl.novi.eindopdracht.entities;
 
-public class PerformerProfile {
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "performers")
+public class PerformerProfile extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "instrument_id")
+    private Instrument instrument;
+
+    @OneToMany(mappedBy = "performer")
+    private List<PerformerAct> acts = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "person_id")
+    private Person person;
+
+    public Instrument getInstrument() {
+        return instrument;
+    }
+
+    public void setInstrument(Instrument instrument) {
+        this.instrument = instrument;
+    }
+
+    public List<PerformerAct> getActs() {
+        return acts;
+    }
+
+    public void setActs(List<PerformerAct> acts) {
+        this.acts = acts;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
 }
