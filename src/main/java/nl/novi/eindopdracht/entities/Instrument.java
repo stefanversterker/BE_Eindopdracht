cd136dbb2831a846d683c2a480a6b4aaafc7d888
stefanversterker <stefanversterker@gmail.com>
2026-07-04 13:11:26 +0200

    created Instrument entity

--- src/main/java/nl/novi/eindopdracht/entities/Instrument.java
+++ src/main/java/nl/novi/eindopdracht/entities/Instrument.java
@@ -1,4 +1,34 @@
 package nl.novi.eindopdracht.entities;
 
-public class Instrument {
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "instruments")
+public class Instrument extends BaseEntity {
+
+    private String name;
+
+    @OneToMany(mappedBy = "instrument")
+    private List<PerformerProfile> performerProfiles = new ArrayList<>();
+
+    //Getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<PerformerProfile> getPerformerProfiles() {
+        return performerProfiles;
+    }
+
+    public void setPerformerProfiles(List<PerformerProfile> performerProfile) {
+        this.performerProfiles = performerProfile;
+    }
 }
