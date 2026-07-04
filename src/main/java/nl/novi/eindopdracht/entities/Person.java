062c5ac76f33125dcbbef831c8326acb327d2b96
stefanversterker <stefanversterker@gmail.com>
2026-07-04 15:06:27 +0200

    created Person entity

--- src/main/java/nl/novi/eindopdracht/entities/Person.java
+++ src/main/java/nl/novi/eindopdracht/entities/Person.java
@@ -1,4 +1,65 @@
 package nl.novi.eindopdracht.entities;
 
-public class Person {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "persons")
+public abstract class Person extends BaseEntity {
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @OneToOne(mappedBy = "person")
+    private PerformerProfile performerProfile;
+
+    //Getters and setters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public PerformerProfile getPerformerProfile() {
+        return performerProfile;
+    }
+
+    public void setPerformerProfile(PerformerProfile performerProfile) {
+        this.performerProfile = performerProfile;
+    }
 }
