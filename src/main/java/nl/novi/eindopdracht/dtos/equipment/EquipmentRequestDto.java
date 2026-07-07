c6a0258b70bf3b952424d1b37593e8ffd021d62d
stefanversterker <stefanversterker@gmail.com>
2026-07-07 12:26:34 +0200

    created equipment dtos

---
+++ src/main/java/nl/novi/eindopdracht/dtos/equipment/EquipmentRequestDto.java
@@ -0,0 +1,35 @@
+package nl.novi.eindopdracht.dtos.equipment;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class EquipmentRequestDto {
+
+    @NotBlank(message = "Brand is required.")
+    @Size(min = 2, max = 50,
+            message = "Brand must be between 2 and 50 characters")
+    private String brand;
+
+    @NotBlank(message = "Model is required.")
+    @Size(min = 2, max = 50,
+            message = "Model must be between 2 and 50 characters")
+    private String model;
+
+    //Getters and Setters
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
---
+++ src/main/java/nl/novi/eindopdracht/dtos/equipment/EquipmentResponseDto.java
@@ -0,0 +1,37 @@
+package nl.novi.eindopdracht.dtos.equipment;
+
+public class EquipmentResponseDto {
+
+    private Long id;
+
+    private String brand;
+
+    private String model;
+
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
