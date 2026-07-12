e28aa503664724d59a7d91c92cfe57b508bfca0c
stefanversterker <stefanversterker@gmail.com>
2026-07-12 17:26:25 +0200

    created PerformerInstrument repository

--- src/main/java/nl/novi/eindopdracht/repositories/PerformerInstrumentRepository.java
+++ src/main/java/nl/novi/eindopdracht/repositories/PerformerInstrumentRepository.java
@@ -1,9 +1,9 @@
 package nl.novi.eindopdracht.repositories;
 
-import nl.novi.eindopdracht.entities.ActEntity;
+import nl.novi.eindopdracht.entities.PerformerInstrumentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ActRepository extends JpaRepository<ActEntity, Long> {
+public interface PerformerInstrumentRepository extends JpaRepository<PerformerInstrumentEntity, Long> {
 }
