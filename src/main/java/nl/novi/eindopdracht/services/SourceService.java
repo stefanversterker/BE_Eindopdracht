ccc8035938b5867348896cc85fb7735a88023174
stefanversterker <stefanversterker@gmail.com>
2026-07-28 12:38:42 +0200

    created SourceService

--- src/main/java/nl/novi/eindopdracht/services/SourceService.java
+++ src/main/java/nl/novi/eindopdracht/services/SourceService.java
@@ -3,10 +3,8 @@ package nl.novi.eindopdracht.services;
 import nl.novi.eindopdracht.dtos.source.SourceRequestDto;
 import nl.novi.eindopdracht.dtos.source.SourceResponseDto;
 import nl.novi.eindopdracht.entities.PerformerInstrumentEntity;
-import nl.novi.eindopdracht.entities.PerformerProfileEntity;
 import nl.novi.eindopdracht.entities.SourceEntity;
 import nl.novi.eindopdracht.exceptions.RecordNotFoundException;
-import nl.novi.eindopdracht.mappers.PerformerInstrumentDtoMapper;
 import nl.novi.eindopdracht.mappers.SourceDtoMapper;
 import nl.novi.eindopdracht.repositories.PerformerInstrumentRepository;
 import nl.novi.eindopdracht.repositories.SourceRepository;
@@ -21,18 +19,17 @@ public class SourceService {
     private final SourceRepository sourceRepository;
     private final SourceDtoMapper sourceDtoMapper;
     private final PerformerInstrumentRepository performerInstrumentRepository;
-    private final PerformerInstrumentDtoMapper performerInstrumentDtoMapper;
+
 
 
     public SourceService(
             SourceRepository sourceRepository,
             SourceDtoMapper sourceDtoMapper,
-            PerformerInstrumentRepository performerInstrumentRepository,
-            PerformerInstrumentDtoMapper performerInstrumentDtoMapper) {
+            PerformerInstrumentRepository performerInstrumentRepository) {
         this.sourceRepository = sourceRepository;
         this.sourceDtoMapper = sourceDtoMapper;
         this.performerInstrumentRepository = performerInstrumentRepository;
-        this.performerInstrumentDtoMapper = performerInstrumentDtoMapper;
+
     }
 
     @Transactional(readOnly = true)
@@ -51,8 +48,20 @@ public class SourceService {
     public SourceResponseDto createSource(SourceRequestDto sourceRequestDto) {
         // Create the entity the repository expects
         SourceEntity sourceEntity = sourceDtoMapper.mapToEntity(sourceRequestDto);
+
+        // Extract PerformerInstrumentId
+        Long performerInstrumentId = sourceRequestDto.getPerformerInstrumentId();
+
+        // Find PerformerInstrumentEntity
+        PerformerInstrumentEntity performerInstrument = performerInstrumentRepository.findById(performerInstrumentId)
+                .orElseThrow(() -> new RecordNotFoundException("PerformerInstrument with id " + performerInstrumentId + " not found."));
+
+        // Set related PerformerInstrument
+        sourceEntity.setPerformerInstrumentEntity(performerInstrument);
+
         // Save the entity in the repository
         sourceEntity = sourceRepository.save(sourceEntity);
+
         // Convert the saved entity to a response DTO
         return sourceDtoMapper.mapToDto(sourceEntity);
     }
@@ -61,7 +70,7 @@ public class SourceService {
         // Retrieve the entity from the database with its current values
         SourceEntity existingSourceEntity = getSourceEntity(id);
 
-        // Update field
+        // Update Name field
         existingSourceEntity.setName(sourceRequestDto.getName());
 
         // Extract PerformerInstrumentId
@@ -71,7 +80,8 @@ public class SourceService {
         PerformerInstrumentEntity performerInstrument = performerInstrumentRepository.findById(performerInstrumentId)
                 .orElseThrow(() -> new RecordNotFoundException("PerformerInstrument with id " + performerInstrumentId + " not found."));
 
-        existingSourceEntity.setPerformerInstrumentEntity(performerInstrumentRepository.getPerformerInstrumentId());
+        // Update PerformerInstrument field
+        existingSourceEntity.setPerformerInstrumentEntity(performerInstrument);
 
         // Save update to repository
         existingSourceEntity = sourceRepository.save(existingSourceEntity);
@@ -82,15 +92,13 @@ public class SourceService {
 
     public void deleteSource(Long id) {
         SourceEntity source = getSourceEntity(id);
-        // If there are any relations, remove these first by setting the fields to null.
         sourceRepository.delete(source);
     }
 
     // Helper: gets entity from repository
     private SourceEntity getSourceEntity(Long id) {
         SourceEntity sourceEntity = sourceRepository.findById(id)
-                .orElseThrow(() -> new RecordNotFoundException("Source " + id + " not found"));
+                .orElseThrow(() -> new RecordNotFoundException("Source with id " + id + " not found"));
         return sourceEntity;
     }
-
 }
