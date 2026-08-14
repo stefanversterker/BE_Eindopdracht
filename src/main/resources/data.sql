d6eee180df9ae96d3e570c36f51097a46128590a
stefanversterker <stefanversterker@gmail.com>
2026-08-14 16:37:38 +0200

    populated event

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,31 @@
 -- Insert into person
-    insert into person (first_name, last_name, email, phone)
-    values ('Bart', 'Brosens', 'bart@ritalynn.zu', '06 18031976'),
-           ('Mark', 'Schrauwen', 'mark@ritalynn.zu', '06 23061985'),
-           ('Barry', 'Stuijts', 'barry@ritalynn.zu', '06 19081979'),
-           ('Olaf', 'van den Berg', 'olaf@ritalynn.zu', '06 15111984'),
-           ('Stefan', 'Verster', 'stefan@ritalynn.zu' ,'06 24041982'),
-           ('Kees', 'Paneermeel', 'kees@paneermeel.nl', '06 15051965');
+    insert into persons (id, first_name, last_name, email, phone)
+    values (1, 'Bart', 'Brosens', 'bart@ritalynn.zu', '06 18031976'),
+           (2, 'Mark', 'Schrauwen', 'mark@ritalynn.zu', '06 23061985'),
+           (3, 'Barry', 'Stuijts', 'barry@ritalynn.zu', '06 19081979'),
+           (4, 'Olaf', 'van den Berg', 'olaf@ritalynn.zu', '06 15111984'),
+           (5, 'Stefan', 'Verster', 'stefan@ritalynn.zu' ,'06 24041982'),
+           (6, 'Kees', 'Paneermeel', 'kees@paneermeel.nl', '06 15051965'),
+           (7, 'Rick', 'Paneermeel', 'rick@paneermeel.nl', '06 12281996');
 
 -- Insert into instrument
-    insert into instrument (name)
-    values ('lead vocals'),
-           ('backing vocals'),
-           ('electric bass guitar'),
-           ('acoustic guitar'),
-           ('electric guitar'),
-           ('blues harp'),
-           ('drums');
+    insert into instruments (id, name)
+    values (1, 'lead vocals'),
+           (2, 'backing vocals'),
+           (3, 'electric bass guitar'),
+           (4, 'acoustic guitar'),
+           (5, 'electric guitar'),
+           (6, 'blues harp'),
+           (7, 'drums');
 
 -- Insert into event
+    insert into events (id, date, venue)
+    values (1, 2027-07-23, 'Breda Barst'),
+           (2, 2027-06-29, 'Van Gogh Live'),
+           (3, 2027-08-02, 'Trailerfest');
+
+
+
 
 -- Insert into act
 
