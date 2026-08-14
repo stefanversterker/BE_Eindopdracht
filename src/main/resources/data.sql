39c5664cb8d2fc89f3d9ccb46f24383c49e7e345
stefanversterker <stefanversterker@gmail.com>
2026-08-14 17:13:04 +0200

    populated acts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,7 +6,12 @@
            (4, 'Olaf', 'van den Berg', 'olaf@ritalynn.zu', '06 15111984'),
            (5, 'Stefan', 'Verster', 'stefan@ritalynn.zu' ,'06 24041982'),
            (6, 'Kees', 'Paneermeel', 'kees@paneermeel.nl', '06 15051965'),
-           (7, 'Rick', 'Paneermeel', 'rick@paneermeel.nl', '06 12281996');
+           (7, 'Rick', 'Paneermeel', 'rick@paneermeel.nl', '06 12281996'),
+           (8, 'Abi', 'Borcic', 'abi@rbpine.nl', '06 63547483'),
+           (9, 'Rens', 'Geubbels', 'rens@gmail.com', '06 49724952'),
+           (10, 'Leon', 'van den Langenberg', 'leon@fifihengsten.nl', '456787834'),
+           (11, 'Wies', 'Arts', 'wies@fifihengsten.nl', '06 99834756'),
+           (12, 'Rop', 'van de Laar', 'rop@fifihengsten.nl', '06 74673392');
 
 -- Insert into instrument
     insert into instruments (id, name)
@@ -16,7 +21,7 @@
            (4, 'acoustic guitar'),
            (5, 'electric guitar'),
            (6, 'blues harp'),
-           (7, 'drums');
+           (7, 'drum kit');
 
 -- Insert into event
     insert into events (id, date, venue)
@@ -24,10 +29,12 @@
            (2, 2027-06-29, 'Van Gogh Live'),
            (3, 2027-08-02, 'Trailerfest');
 
-
-
-
 -- Insert into act
+    insert into acts (id, name, phone, email)
+    values (1, 'Rita Lynn', '06 45188510', 'info@ritalynn.zu'),
+           (2, 'R.B. Pine', '06 63547483', 'info@rbpine.nl'),
+           (3, 'Fifi Hengsten', '06 53889223', 'info@fifihengsten.nl')
+
 
 -- Insert into equipment
 
