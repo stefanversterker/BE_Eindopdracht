d488d822d1bd60240c3e0f8e62d2e9fa3ba36c80
stefanversterker <stefanversterker@gmail.com>
2026-08-16 13:42:02 +0200

    populated channels

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,9 +7,9 @@
            (5, 'Stefan', 'Verster', 'stefan@gmail.com' ,'06 24041982'),
            (6, 'Abi', 'Borcic', 'abi@gmail.com', '06 63547483'),
            (7, 'Rens', 'Geubbels', 'rens@gmail.com', '06 49724952'),
-           (8, 'Leon', 'van den Langenberg', 'leon@gmail.com', '456787834'),
-           (9, 'Wies', 'Arts', 'wies@gmail.com', '06 99834756'),
-           (10, 'Rop', 'van de Laar', 'rop@gmail.com', '06 74673392'),
+           (8, 'Arne', 'Martens', 'arne@gmail.com', '456787834'),
+           (9, 'Erik', 'Griffioen', 'erik@gmail.com', '06 99834756'),
+           (10, 'Henning', 'van Laarhoven', 'henning@gmail.com', '06 74673392'),
            (11, 'Suzanne', 'van der Leeden', 'suzanne@soundrental.nl', '06 15051965'),
            (12, 'Fien', 'Verster', 'fien@soundrental.nl', '06 12281996'),
            (13, 'Elise', 'Verheyden', 'elise@soundrental.nl', '06 12281996');
@@ -26,9 +26,9 @@
 
 -- Insert into event
     insert into events (id, date, venue)
-    values (1, 2027-07-23, 'Breda Barst'),
-           (2, 2027-06-29, 'Van Gogh Live'),
-           (3, 2027-08-02, 'Trailerfest');
+    values (1, 2027-07-23, 'Mezz'),
+           (2, 2027-06-29, 'cafe de Kerel'),
+           (3, 2027-08-02, 'Electron');
 
 -- Insert into act
     insert into acts (id, name, phone, email)
@@ -55,9 +55,9 @@
            (15, 'AKG', 'C414 XLS'),
            (16, 'AKG', 'C411 PP'),
            (17, 'AKG', 'C411 PP'),
-           (18, 'Allen & Heath', 'CQ18T'),
-           (19, 'Allen & Heath', 'QU-5D'),
-           (20, 'Allen & Heath', 'QU-7D');
+           (18, 'Mackie', 'ProFX22v3'),
+           (19, 'Mackie', 'Onyx24'),
+           (20, 'Mackie', '1604 VLZ4');
 
 
 -- Insert into microphones
@@ -81,7 +81,7 @@
            (17, true);
 
 -- Insert into microphone_polar_patterns
-    insert into microphone_polar_patterns (microphone_id, polar_pattern)
+    insert into microphone_polar_patterns (id, polar_pattern)
     values (1, CARDIOID),
            (2, CARDIOID),
            (3, CARDIOID),
@@ -132,10 +132,108 @@
            (3, 13, KLEINE_BUS);
 
 -- Insert into performer-instrument (needs performer-profile + instrument)
+    insert into performer_instruments (id, performer_profile_id, instrument_id)
+    values (1, 1, 1),
+           (2, 1, 4),
+           (3, 1, 5),
+           (4, 2, 2),
+           (5, 2, 6),
+           (6, 3, 2),
+           (7, 3, 3),
+           (8, 4, 4),
+           (9, 4, 5),
+           (10, 5, 7),
+           (11, 6, 1),
+           (12, 6, 5),
+           (13, 7, 3),
+           (14, 8, 2),
+           (15, 8, 5),
+           (16, 9, 1),
+           (17, 9, 6),
+           (18, 10, 3);
 
 -- Insert into source (needs performer-instrument)
+    insert into sources (id, performer_instrument_id, name)
+    values (1, 1, 'lead vocals Bart'),
+           (2, 2, 'acoustic guitar Bart'),
+           (3, 3, 'electric guitar Bart'),
+           (4, 4, 'bgv Mark'),
+           (5, 5, 'blues harp left Mark'),
+           (6, 5, 'blues harp right Mark'),
+           (7, 6, 'bgv Barry'),
+           (8, 7, 'electric bass Barry'),
+           (9, 8, 'acoustic guitar Olaf'),
+           (10, 9, 'electric guitar Olaf'),
+           (11, 10, 'kick'),
+           (12, 10, 'snare'),
+           (13, 10, 'rack tom'),
+           (14, 10, 'floor tom'),
+           (15, 10, 'hihat'),
+           (16, 10, 'overhead left'),
+           (17, 10, 'overhead right'),
+           (18, 11, 'lead vocals Abi'),
+           (19, 12, 'electric guitar Abi'),
+           (20, 13, 'electric bass Rens'),
+           (21, 14, 'bgv Arne'),
+           (22, 15, 'electric guitar Arne'),
+           (23, 16, 'lead vocals harp Erik'),
+           (24, 17, 'blues harp Erik'),
+           (25, 18, 'electric bass Henning');
+
 
 -- Insert into channel (needs mixer + optional source)
+    insert into channels (id, mixer_id, number, source_id)
+    values (1,18,1,11),
+           (2,18,2,12),
+           (3,18,3,13),
+           (4,18,4,14),
+           (5,18,5,15),
+           (6,18,6,16),
+           (7,18,7,17),
+           (8,18,8,20),
+           (9,18,9,18),
+           (10,18,10,19),
+           (11,18,11, null),
+           (12,18,12, null),
+           (13,18,13, null),
+           (14,18,14, null),
+           (15,18,15, null),
+           (16,18,16, null),
+           (17,18,17, null),
+           (18,19,1,11),
+           (19,19,2,12),
+           (20,19,3,13),
+           (21,19,4,14),
+           (22,19,5,15),
+           (23,19,6,16),
+           (24,19,7,17),
+           (25,19,8,8),
+           (26,19,9,1),
+           (27,19,10,4),
+           (28,19,11,7),
+           (29,19,12,2),
+           (30,19,13,3),
+           (31,19,14,9),
+           (32,19,15,10),
+           (33,19,16,5),
+           (34,19,17,6),
+           (35,19,18, null),
+           (36,20,1,11),
+           (37,20,2,12),
+           (38,20,3,13),
+           (39,20,4,14),
+           (40,20,5,15),
+           (41,20,6,16),
+           (42,20,7,17),
+           (43,20,8,25),
+           (44,20,9,23),
+           (45,20,10,21),
+           (46,20,11,24),
+           (47,20,12,22),
+           (48,20,13, null),
+           (49,20,14, null),
+           (50,20,15, null),
+           (51,20,16, null);
 
 -- Insert into performance (needs event + act)
 
