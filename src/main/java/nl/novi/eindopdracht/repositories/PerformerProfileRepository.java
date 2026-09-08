457f6776037d44475ae546ac2cb3c97cec829ade
stefanversterker <stefanversterker@gmail.com>
2026-09-08 18:46:56 +0200

    implemented DuplicateRecordException in PerformerProfile

--- src/main/java/nl/novi/eindopdracht/repositories/PerformerProfileRepository.java
+++ src/main/java/nl/novi/eindopdracht/repositories/PerformerProfileRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PerformerProfileRepository extends JpaRepository<PerformerProfileEntity, Long> {
+
+    boolean existsByPersonEntityId(Long personId);
+
 }
